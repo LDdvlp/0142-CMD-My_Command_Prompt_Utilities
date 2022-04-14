6f6bf147977c1de85495ad591a4e618bdf92f2c5
LDdvlp <loicdrouet@gmail.com>
2022-04-14 17:27:05 +0200

    v.1.0.1 : Ajout de 3 commandes 12, 13 et 14

--- myCommandPromptUtilities.bat
+++ myCommandPromptUtilities.bat
@@ -1,3 +1,9 @@
+@ECHO OFF
+REM @ECHO OFF en ligne 1 pour désactiver l' écho
+REM et l'affichage de l'invite de commandes,
+REM avec @ pour ne pas afficher la commande ECHO OFF elle même.
+
+REM *****************************************************************************************************************************************
 REM
 REM ___  ___        _____                                           _  ______                          _     _   _ _   _ _ _ _   _            
 REM |  \/  |       /  __ \                                         | | | ___ \                        | |   | | | | | (_) (_) | (_)           
@@ -7,9 +13,19 @@ REM | |  | | |_| | | \__/\ (_) | | | | | | | | | | | (_| | | | | (_| | | |  | | 
 REM \_|  |_/\__, |  \____/\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|\__,_| \_|  |_|  \___/|_| |_| |_| .__/ \__|  \___/ \__|_|_|_|\__|_|\___||___/ 
 REM          __/ |                                                                              | |                                           
 REM         |___/                                                                               |_|                                           
-REM 
+REM
+REM *****************************************************************************************************************************************
 
-@ECHO OFF
+REM Récupération du code page actuel dans une variable,
+REM généralement 850 pour encodage en Multilingual (Latin I)
+REM Stockage de la sortie de commande CHCP dans une variable avec une boucle FOR
+REM IMPORTANT !!! : en invite de commande, mettre %a et dans un fichier mettre %%a
+REM CHCP : CHange Code Page
+FOR /F "usebackq delims==" %%a IN (`CHCP`) DO SET OLDCHCP=%%a
+
+REM Changement du Code Page en 65001 pour encodage en UTF-8 (universel, surtout pour les accents)
+SET NEWCHCP=65001
+CHCP %NEWCHCP%
 
 :startmenu
 COLOR 1B
@@ -23,17 +39,20 @@ ECHO    *   Utilities Menu   *
 ECHO    **********************
 ECHO.
 ECHO    ----------------------
-ECHO    01. Windows Version (Window)
-ECHO    02. Windows Informations (Prompt)
-ECHO    03. System Informations (Window)
-ECHO    04. System Informations (Prompt)
-ECHO    05. List Temp Directory (Prompt)
-ECHO    06. Empty Temp Directory Content - Admin Only (Prompt)
-ECHO    07. Windows User Accounts (Window)
-ECHO    08. Windows Applications (Window)
-ECHO    09. Windows Printers (Window)
-ECHO    10. WiFi Keys (Prompt)
-ECHO    11. DirectX Diagnostic Tool - DxDiag (Window)
+ECHO    01. Windows Version                                     - (Window)
+ECHO    02. Windows Informations                                - (Prompt)
+ECHO    03. System Informations                                 - (Window)
+ECHO    04. System Informations                                 - (Prompt)
+ECHO    05. List Temp Directory                                 - (Prompt)
+ECHO    06. Empty Temp Directory Content - [Admin Only Started] - (Prompt)
+ECHO    07. Windows User Accounts                               - (Window)
+ECHO    08. Windows Applications                                - (Window)
+ECHO    09. Windows Printers                                    - (Window)
+ECHO    10. WiFi Keys                                           - (Prompt)
+ECHO    11. DirectX Diagnostic Tool, DxDiag                     - (Window)
+ECHO    12. Désinstaller ou modifier un programme               - (Window)
+ECHO    13. Informations d’identification stockées              - (Prompt)
+ECHO    14. Code Page - Encodage du texte affiché               - (Prompt)
 ECHO    ----------------------
 ECHO    99. Backend
 ECHO    ----------------------
@@ -53,6 +72,9 @@ IF /I "%choice1%"=="08" (GOTO winapps)
 IF /I "%choice1%"=="09" (GOTO winprinters)
 IF /I "%choice1%"=="10" (GOTO wifikeys)
 IF /I "%choice1%"=="11" (GOTO dxdiag)
+IF /I "%choice1%"=="12" (GOTO appwiz)
+IF /I "%choice1%"=="13" (GOTO cmdkeylist)
+IF /I "%choice1%"=="14" (GOTO chcp)
 IF /I "%choice1%"=="99" (GOTO backendmenu)
 IF /I "%choice1%"=="00" (GOTO quitmenu)
 
@@ -146,10 +168,49 @@ GOTO startmenu
 dxdiag 
 GOTO startmenu
 
+:: 012 - Désinstaller ou modifier un programme
+:appwiz
+appwiz.cpl
+GOTO startmenu
+
+:: 013 - Informations d’identification actuellement stockées
+:cmdkeylist
+COLOR 1F
+CLS
+ECHO.
+ECHO    ***********************************************
+ECHO    *   Informations d’identification  stockées   *
+ECHO    ***********************************************
+ECHO.
+cmdkey /list
+CALL :whenready
+GOTO startmenu
+
+:: 014 - Informations d’identification actuellement stockées
+:chcp
+COLOR 1F
+CLS
+ECHO.
+ECHO    *****************************************************************
+ECHO    *   Encodage du texte affiché avant le démarrage du programme   *
+ECHO    *****************************************************************
+ECHO.
+ECHO    %OLDCHCP%
+ECHO.
+ECHO    ******************************************
+ECHO    *   Encodage du texte affiché en cours   *
+ECHO    ******************************************
+ECHO.
+ECHO    Page de codes active : %NEWCHCP%
+ECHO.
+CALL :whenready
+
+GOTO startmenu
+
 :: 099 - Backend
 :backendmenu
 COLOR 4E
-CLS
+CLS 
 ECHO.
 GOTO menu2
 
